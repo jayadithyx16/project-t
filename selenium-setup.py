ac61178b28c9cd3f6bd164a3d4dfe140446eb070
Jayadithya <114508091+jayadithyx16@users.noreply.github.com>
2023-11-11 12:00:41 +0530

    Update selenium-setup.py

--- selenium-setup.py
+++ selenium-setup.py
@@ -1,11 +1,21 @@
 import time
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import os
 import time
-os.environ['PATH']+=r'C:\Users\rjaya\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe'
-
-driver =webdriver.Chrome()
+from webdriver_manager.chrome import ChromeDriverManager
+webdriver_service = Service(ChromeDriverManager().install())
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 driver.get("https://moviesmod.vip/")
+fmovie=driver.find_element(By.CSS_SELECTOR,"#post-123004 > header > h2 > a")
+print(fmovie.text)
 print('doneeee')
 time.sleep(4)
+
+
