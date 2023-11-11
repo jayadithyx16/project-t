76c8660a820b71b28d34d68dd640a8b6571745c6
Jayadithya <114508091+jayadithyx16@users.noreply.github.com>
2023-11-11 11:31:31 +0530

    Add files via upload

---
+++ selenium-setup.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+import os
+import time
+os.environ['PATH']+=r'C:\Users\rjaya\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe'
+
+driver =webdriver.Chrome()
+driver.get("https://moviesmod.vip/")
+print('doneeee')
+time.sleep(4)
